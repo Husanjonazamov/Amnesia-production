0aa9ffb4ffaea3ef374e9f8abe5d7d9b7f3a0c87
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-06-24 14:21:08 +0500

    New g4f version: 0.5.5.5 (current: 0.3.9.7) | pip install -U g4f

--- core/apps/havasbook/serializers/book/currency.py
+++ core/apps/havasbook/serializers/book/currency.py
@@ -14,10 +14,8 @@ def convert_currency(amount: Decimal, to_currency: str) -> Decimal:
         url = EXCHANGE_URL
         response = requests.get(url, timeout=3)
         data = response.json()
-        logging.error(f"--------\n\n{url}\n\n-----------")
         
         if data.get("result") != "success":
-            print("Currency API failed")
             return round(amount, 2)
 
         rate = data.get("conversion_rates", {}).get(to_currency.upper())
@@ -27,5 +25,4 @@ def convert_currency(amount: Decimal, to_currency: str) -> Decimal:
         return round(amount * Decimal(str(rate)), 2)
     
     except Exception as e:
-        print("Currency conversion error:", e)
         return round(amount, 2)
